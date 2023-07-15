47c93896e0cd1a6c04da318131d87d746ad80211
priyajawariya <priya@techmeraki.com>
2023-07-15 09:36:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import Connection from './database/db.js';
+import route from './routes/route.js';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Your code here
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', route);
+
+const PORT = 3000;
+Connection();
+
+app.listen(PORT,()=>console.log(`Your server is running successfully ${PORT}`))
